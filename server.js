9feb780836a36c6c27d4123c1b766af3ae8d96b2
Hack Reactor Students <github@hackreactor.com>
2013-05-26 12:06:45 -0700

    Packaging for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express'),
-  fs = require('fs'),
-  url = require('url'),
   app = express();
 
 app.get('*', function (req, res) {
   res.sendfile(__dirname + '/www/' + req.url);
 });
 
-app.listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server running at' + port);
+});
